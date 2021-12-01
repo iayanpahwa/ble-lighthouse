61e8854a9d60fbb9097fe192fbd8d0e5fe11d81e
iayanpahwa <iayanpahwa@gmail.com>
2021-12-01 17:43:44 +0530

    add script to deploy ble beacon

---
+++ main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#set -x
+#sudo PyBeacon -i 01234567890123456789012345678901
+#sudo PyBeacon -u https://codeNsolder.com
+
+echo $ADVERTISE
+echo $URL
+
+if [ "$ADVERTISE" == "TRUE" ]
+then
+	echo "creating ble beacon with url: $URL"
+	hciconfig hci0 leadv 3
+	hciconfig hci0 noscan
+	PyBeacon -u $URL
+	sleep 1
+else
+# Turn off advertising mode
+	hciconfig hci0 noleadv
+fi
+
+
+
+
+
