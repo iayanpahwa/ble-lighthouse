019bec9b9916b6ac6aed8e20e5e390693f42f1da
iayanpahwa <iayanpahwa@gmail.com>
2021-12-02 14:17:17 +0530

    add support for UUID beacon broadcast

--- main.sh
+++ main.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 #set -x
-#sudo PyBeacon -i 01234567890123456789012345678901
-#sudo PyBeacon -u https://codeNsolder.com
 
 echo $ADVERTISE
+echo $BEACON_TYPE
 echo $URL
+echo $UUID
 
 if [ "$ADVERTISE" == "TRUE" ]
 then
-	echo "creating ble beacon with url: $URL"
-	hciconfig hci0 leadv 3
-	hciconfig hci0 noscan
-	PyBeacon -u $URL 2> /dev/null
-	sleep 1
+	if [ "$BEACON_TYPE" == "URL" ]
+	then
+		echo "creating ble beacon with url: $URL"
+		hciconfig hci0 leadv 3
+		hciconfig hci0 noscan
+		PyBeacon -u $URL 2> /dev/null
+		sleep 1
+	elif [ "$BEACON_TYPE" == "UUID" ]
+	then
+		echo "creating ble beacon with UUID: $UUID"
+		hciconfig hci0 leadv 3
+		hciconfig hci0 noscan
+		PyBeacon -i $UUID 2> /dev/null
+	else
+		echo "Undefined BEACON_TYPE"
+	fi
 else
 # Turn off advertising mode
 	hciconfig hci0 noleadv
 fi
 
 while true; do sleep 5000; done
-
-
-
